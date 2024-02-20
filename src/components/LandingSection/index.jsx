8d7ea6409805ed06c7814aed64aaf608098f7cc6
mBukator <m.bukator03@gmail.com>
2024-02-20 17:51:21 +0200

    style: Changed Landing Section styles

--- src/components/LandingSection/index.jsx
+++ src/components/LandingSection/index.jsx
@@ -10,22 +10,28 @@ import styles from './LandingSection.module.css'
 const LandingSection = () => {
 	return (
 		<div className={styles.container}>
-			<div className={styles.landing}>
-				<div className={styles.text}>
-					<p className={styles.text_lg}>
-						RECORD VIDEO.
-						<br />
-						SHARE IT TO EVERYONE IN THE WORLD
-					</p>
-					<p className={styles.text_sm}>
-						Got an idea for video? Record it! Want to share it with others? We have solution!
-					</p>
-					<Button title={'GET STARTED'} width={332} height={80} insetValue={'22em'} />
-				</div>
+			<div className={styles.wrapper}>
+				<div className={styles.landing}>
+					<div className={styles.text}>
+						<p className={styles.text_lg}>
+							RECORD VIDEO.
+							<br />
+							SHARE IT TO EVERYONE IN THE WORLD
+						</p>
+						<p className={styles.text_sm}>
+							Got an idea for video? Record it! Want to share it with others? We have solution!
+						</p>
+						<Button title={'GET STARTED'} width={332} height={80} insetValue={'22em'} />
+					</div>
 
-				<div className={styles.imgs}>
-					<img src={monitor} alt='monitor' className={`${styles.monitor} ${styles.anim_obj}`} />
-					<img src={keyboard} alt='keyboard' className={`${styles.keyboard} ${styles.anim_obj}`} />
+					<div className={styles.imgs}>
+						<img src={monitor} alt='monitor' className={`${styles.monitor} ${styles.anim_obj}`} />
+						<img
+							src={keyboard}
+							alt='keyboard'
+							className={`${styles.keyboard} ${styles.anim_obj}`}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
