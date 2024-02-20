bd92693e55b26850942d041e0054285dab0f3551
mBukator <m.bukator03@gmail.com>
2024-02-20 20:22:02 +0200

    Created card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './Card.module.css'
+
+const Card = ({ width, height, title, text, icon }) => {
+	const containerStyle = {
+		width: `${width}px`,
+		height: `${height}px`,
+	}
+
+	return (
+		<div className={styles.container} style={containerStyle}>
+			<div className={styles.wrapper}>
+				<div className={styles.icon_container}>
+					<img src={icon} alt='icon' className={styles.icon} />
+				</div>
+				<div className={styles.title_container}>
+					<h1 className={styles.title}>{title.toUpperCase()}</h1>
+				</div>
+				<p className={styles.text}>{text}</p>
+			</div>
+		</div>
+	)
+}
+
+export default Card
