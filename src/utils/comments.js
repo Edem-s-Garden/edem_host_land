96ed83d11d5788368b8f4ca76c6b2a85b3240c76
mosyamosya <93034139+mosyamosya@users.noreply.github.com>
2024-02-21 19:55:12 +0200

    Feedback. Add comments

---
+++ src/utils/comments.js
@@ -0,0 +1,45 @@
+import monkey1 from '../icons/users/monkey1.png'
+import monkey2 from '../icons/users/monkey2.png'
+import monkey3 from '../icons/users/monkey3.png'
+import monkey4 from '../icons/users/monkey4.png'
+import monkey5 from '../icons/users/monkey5.png'
+import monkey6 from '../icons/users/monkey6.png'
+
+export const comments = [
+	{
+		id: 1,
+		title: 'Alice Black',
+		text: "Absolutely love the variety of content on Edem's Garden!",
+		icon: monkey1,
+	},
+	{
+		id: 2,
+		title: 'Bob Warner',
+		text: 'The production quality is top-notch! The videos are not only informative but also visually appealing.',
+		icon: monkey2,
+	},
+	{
+		id: 3,
+		title: 'Carol Scott',
+		text: 'The streaming quality is impeccable, and I love the vast library of videos available.',
+		icon: monkey3,
+	},
+  {
+		id: 4,
+		title: 'Dave Smith',
+		text: "I can catch all my favorite games in real-time, and the video quality is fantastic. No more missing out on crucial moments.",
+		icon: monkey4,
+	},
+	{
+		id: 5,
+		title: 'Eve Walter',
+		text: "The download feature is a lifesaver for when I'm on the go or facing slow internet.",
+		icon: monkey5,
+	},
+	{
+		id: 6,
+		title: 'Fillip Edison',
+		text: 'It understands my preferences and suggests content that I genuinely enjoy.',
+		icon: monkey6,
+	},
+]
