bd7f5dd4fa4a284b5088426159f10e7c84cefdbb
mosyamosya <93034139+mosyamosya@users.noreply.github.com>
2024-02-21 19:52:36 +0200

    Feedback

---
+++ src/components/Feedback/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { comments } from '../../utils/comments'
+import Field from '../Field'
+import Divider from '../ui/'
+import styles from './Feedback.module.css'
+import Button from '../ui/Button'
+
+function Feedback() {
+	return (
+		<div id='feedback' className={styles.container}>
+			<Divider title={"REVIEWS"} />
+			<div className='field_container'>
+				<div className={styles.comments}>
+					{comments.slice(0, comments.length / 2).map((comment) => {
+						return (
+							<Field
+								key={comment.id}
+								title={comment.title}
+								text={comment.text}
+								icon={comment.icon}
+								width={428}
+								height={164}
+							/>
+						)
+					})}
+				</div>
+				<div className={styles.comments}>
+					{comments.slice(comments.length / 2, comments.length).map((comment) => {
+						return (
+							<Field
+								key={comment.id}
+								title={comment.title}
+								text={comment.text}
+								icon={comment.icon}
+								width={428}
+								height={164}
+							/>
+						)
+					})}
+				</div>
+			</div>
+			<div className={styles.button}>
+				<Button title={'GET STARTED'} width={532} height={80} insetValue={'35em'} />
+			</div>
+		</div>
+	)
+}
+
+export default Feedback
